01c9492567fc81729dade8a26d21665cd22e9ebb
Marilena Alexe <marilena.alexe@gmail.com>
2017-07-11 17:33:46 +0100

    start point code

---
+++ client/src/app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+window.addEventListener('load', function () {
+  ReactDOM.render(
+    <h3>Application</h3>,
+    document.getElementById('app')
+  );
+});
